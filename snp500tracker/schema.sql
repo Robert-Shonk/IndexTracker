7508c5c0f9af5c9d40cd6ee895d30527738b497a
Robert Shonk <rshonkcode@gmail.com>
2025-04-07 21:46:42 -0700

    Updated snp table. Removed UNIQUE constraint on symbol.

--- snp500tracker/schema.sql
+++ snp500tracker/schema.sql
@@ -3,14 +3,14 @@ DROP TABLE IF EXISTS stock;
 
 CREATE TABLE snp (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    symbol TEXT UNIQUE NOT NULL,
+    symbol TEXT NOT NULL, -- symbol not unique. list/table will be updated and date recorded will be added along full new list.
     security TEXT NOT NULL,
     sector TEXT NOT NULL,
     sub_industry TEXT NOT NULL,
     hq_location TEXT NOT NULL,
     date_added TEXT NOT NULL, -- date added to S&P500 list
     CIK TEXT NOT NULL,
-    founded INTEGER NOT NULL,
+    founded TEXT NOT NULL,
     date_recorded TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
